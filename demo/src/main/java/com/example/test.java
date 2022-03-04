0308f1dee326b4314b491d40d49dfc080d1c8632
Gonzalo Arana <arana.gonzalo98@gmail.com>
2022-03-04 09:52:10 -0300

    guardar valores precio y modelo

--- demo/src/main/java/com/example/test.java
+++ demo/src/main/java/com/example/test.java
@@ -11,11 +11,15 @@ public class test {
     private static String url = "https://www.demoblaze.com/index.html#";
     public static WebDriver driver = DriverFactory.getDriver();
     
-    
+   
+
     public static void main(String[] args) throws Exception {   
     
         driver.navigate().to(url);
         driver.manage().window().maximize();
+
+        String model;
+        String price;
 
         WebElement cart = driver.findElement(By.xpath("//a[contains(text(),'Laptops')]"));
         cart.click();
@@ -23,6 +27,12 @@ public class test {
         WebElement firstLaptop =  driver.findElement(By.xpath("//a[contains(text(),'Sony vaio i5')]"));
         firstLaptop.click();
         Thread.sleep(2000);
+        
+        
+        price = driver.findElement(By.xpath("//h3[@class='price-container']")).getText();
+        model = driver.findElement(By.xpath("//h2[@class='name']")).getText();
+        System.out.println("Model: " + model + " Price: " + price);
+        
         driver.findElement(By.xpath("//a[contains(text(),'Add to cart')]")).click();
         Thread.sleep(2000);
         driver.quit();
